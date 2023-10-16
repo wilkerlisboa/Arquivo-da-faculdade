4f36f609dd2692b478c750fb94989a7d1d29cc1c
wilker-home <wilkerlisboa.ofc@gmail.com>
2023-10-16 11:14:46 -0300

    add login in screen

--- Linguagem/C/EXERCICIO 03.c
+++ Linguagem/C/EXERCICIO 03.c
@@ -4,10 +4,13 @@
 // TRABALHO: ATIVIDADE 03
 // DESCRIÇÃO: CALCULAR A IDADE DE UMA PESSOA
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 //VARIAVEL 
-int data_atual, data_nascimento, resultado;
-char nome[50];
+int data_atual, data_nascimento, resultado, validar_nome, validar_senha;
+char nome_adm[50] = "wilker lisboa";
+char senha_adm[50] = "lisboa123*";
+char nome[50], senha[50];
 //MENU
 void show(){
 	printf("### ##   ### ###  ####     #### ##    ##              ### ##    ## ##    ## ##   #### ##    \n"); 
@@ -21,22 +24,30 @@ void show(){
     printf("                                                                                            \n");
     printf("    			 CALCULAR A IDADE DE UMA PESSOA												\n\n");
 }
-
 int main(){
-	//PERGUNTA
-	printf("DELTA ROOT> Qual e o seu nome?: ");
+	//LOGIN
+	printf("DELTA ROOT> USUARIO: ");
 	gets(nome);
-	//MENU
-	show();
-	//PERGUNTAR
-	printf("DELTA ROOT> Qual e o ano atual?: ");
-	scanf("%d", &data_atual);
-
-	printf("DELTA ROOT> Qual e o ano de nascimento?: ");
-	scanf("%d", &data_nascimento);
+	printf("DELTA ROOT> SENHA: ");
+	gets(senha);
 	//PROCESSO
-	resultado = data_atual - data_nascimento;
-	//SAIDA
-	printf("DELTA ROOT> %s, sua idade e: %d", nome, resultado);
+	validar_nome = strcmp(nome_adm, nome);
+	validar_senha = strcmp(senha_adm, senha);
+	//DECISÃO
+	if(validar_nome == 0 && validar_senha == 0){
+		//MENU
+		show();
+		// PERGUNTA DO ANO ATUAL
+		printf("DELTA ROOT> Qual e o ano atual?: ");
+		scanf("%d", &data_atual);
+		printf("DELTA ROOT> Qual e o ano de nascimento?: ");
+		scanf("%d", &data_nascimento);
+		//PROCESSO
+		resultado = data_atual - data_nascimento;
+		//SAIDA
+		printf("DELTA ROOT> %s, voce tem %d anos", nome, resultado);
+	}else{
+		printf("USUARIO INVALIDO!!");
+	}; 
 	return 0;
 }
