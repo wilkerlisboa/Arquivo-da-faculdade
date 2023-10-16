df458f7b5d3de8dd0c7281b4b28bd526ff7c1043
wilker-home <wilkerlisboa.ofc@gmail.com>
2023-10-16 11:42:28 -0300

    add login in screen

--- Linguagem/C/EXERCICIO 04.c
+++ Linguagem/C/EXERCICIO 04.c
@@ -5,9 +5,13 @@
 // DESCRIÇÃO: CONVERSOR DE MOEDAS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //VARIAVEL
 float real, dolar, cotacao;
-char nome_usuario[50];
+char nome_usuario[50], senha_usuario[50];
+int validar_nome, validar_senha;
+char nome_adm[50] = "wilker lisboa";
+char senha_adm[50] = "lisboa123*";
 
 //MENU
 void show(){
@@ -26,22 +30,33 @@ void show(){
 //INICIO	
 int main(){
 	system("cls");
-	//PERGUNTA
-	printf("DELTA ROOT> Qual e o seu nome?: ");
+	//LOGIN
+	printf("DELTA ROOT> USUARIO: ");
 	gets(nome_usuario);
-	//MENU
-	show();
-	//PERGUNTA
-	printf("DELTA ROOT> Qual e a cotacao de hoje?: ");
-	scanf("%f", &cotacao);
+	printf("DELTA ROOT> SENHA: ");
+	gets(senha_usuario);
+	//PROCESSO 
+	validar_nome = strcmp(nome_adm, nome_usuario);
+	validar_senha = strcmp(senha_adm, senha_usuario);
+	//DECISÃO
+	if (validar_nome == 0 && validar_senha == 0){
+		//MENU
+		show();
+		//PERGUNTA
+		printf("DELTA ROOT> Qual e a cotacao de hoje?: ");
+		scanf("%f", &cotacao);
 
-	printf("DELTA ROOT> Qual e o valor em R$ ?: ");
-	scanf("%f", &real);
+		printf("DELTA ROOT> Qual e o valor em R$ ?: ");
+		scanf("%f", &real);
 
-	//PROCESSO
-	dolar = cotacao * real;
+		//PROCESSO
+		dolar = cotacao * real;
 
-	//SAIDA
-	printf("DELTA ROOT> O valor em DOLAR e: %.2f $", dolar);
+		//SAIDA
+		printf("DELTA ROOT> O valor em DOLAR e: %.2f $", dolar);
+	}else{
+		printf("USUARIO INVALIDO!!");
+	};
+
 	return 0;
 }
